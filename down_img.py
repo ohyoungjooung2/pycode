96fbb1e301f7d8d538e765f3605aab5cc2b52982
ohyoungjooung <wnapdlf05@gmail.com>
2020-06-07 19:39:53 +0900

    Create down_img.py

---
+++ down_img.py
@@ -0,0 +1,28 @@
+import requests
+import time
+import concurrent.futures
+
+img_urls = [
+    'https://images.unsplash.com/photo-1516117172878-fd2c41f4a759',
+    'https://images.unsplash.com/photo-1532009324734-20a7a5813719',
+    'https://images.unsplash.com/photo-1524429656589-6633a470097c',
+    'https://images.unsplash.com/photo-1530224264768-7ff8c1789d79',
+    'https://images.unsplash.com/photo-1564135624576-c5c88640f235',
+    'https://images.unsplash.com/photo-1541698444083-023c97d3f4b6',
+    'https://images.unsplash.com/photo-1522364723953-452d3431c267',
+]
+
+t1 = time.perf_counter()
+
+for img_url in img_urls:
+    img_bytes = requests.get(img_url).content
+    img_name = img_url.split('/')[3]
+    img_name = f'{img_name}.jpg'
+    with open(img_name,'wb') as img_file:
+         img_file.write(img_bytes)
+         print(f'{img_name} was downloaded...')
+
+
+t2 = time.perf_counter()
+
+print(f'Finished in {t2-t1} seconds')
