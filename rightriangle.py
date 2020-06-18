a21ca034276e73ff568506bf680a2389be7236fa
oyj <oyj@local>
2020-06-19 03:38:00 +0900

    add add * right triangle

--- rightriangle.py
+++ rightriangle.py
@@ -7,3 +7,12 @@ def rtriangle(n):
 
 n=6
 rtriangle(n)
+
+def odd_rtriangle(n):
+    for i in range(1,n+1,2):
+        for _ in range(i):
+           print("*",end='')
+        print()
+
+n=6
+odd_rtriangle(n)
