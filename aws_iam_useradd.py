31713b83f1011dc17fc5e3f403230697b7715f2d
ohyoungjooung <wnapdlf05@gmail.com>
2020-06-18 17:17:07 +0900

    Create aws_iam_useradd.py

---
+++ aws_iam_useradd.py
@@ -0,0 +1,43 @@
+import boto3
+from ohsgpgn import ohsgpgn
+newpass=ohsgpgn.genGoodpass(20)
+username='tester3'
+groupname='testgrp'
+tag_key='Dept'
+tag_val='Entertainment'
+
+client=boto3.client('iam')
+
+#create group
+res = client.create_group(
+    GroupName=groupname
+)
+
+#Create user
+res = client.create_user(
+    UserName=username,
+    Tags = [
+        {
+        'Key': tag_key,
+        'Value': tag_val 
+        }
+    ]
+)
+
+res = client.create_login_profile(
+    UserName=username,
+    Password=newpass,
+    #PasswordResetRequired=True|False
+    PasswordResetRequired=True
+)
+
+res = client.attach_user_policy(
+    UserName='tester3',
+    PolicyArn='arn:aws:iam::aws:policy/IAMUserChangePassword'
+)
+
+#Add user to group
+res = client.add_user_to_group(
+    GroupName=groupname,
+    UserName=username
+)
