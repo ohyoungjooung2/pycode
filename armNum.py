675dbfd2358a22cd85420e87624faa4a3a3b47b1
oyj <oyj@local>
2020-06-23 04:39:34 +0900

    add another armstrong number method

--- armNum.py
+++ armNum.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #n is same with each n**len(str(n))'s sum.
 #153 is armstrong num,cause (1**3)+(5**3)+(3**3)=>153, so 153 is armstrong num.
+#First method
 def armOrNot(n):
     r=0
     sn=str(n)
@@ -12,5 +13,22 @@ def armOrNot(n):
     else:
        print(f'{n} is not armstrong numbber')
 
-for i in range(10000):
-    armOrNot(i)
+#for i in range(10000):
+#    armOrNot(i)
+armOrNot(153)
+
+#Second method
+def isArmstrong(n):
+    r=0
+    nr=n
+    nlen=len(str(n))
+    while(nr != 0):
+        t=nr%10
+        r=r+(t**nlen)
+        nr=nr//10
+    if (r == n):
+        print(f'{n} is armstrong number')
+    else:
+        print(f'{n} is not armstrong number')
+
+isArmstrong(153)
