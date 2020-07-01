2ffc1318b0d0d51b5e457725d76e838de5ad069e
ohyoungjooung <wnapdlf05@gmail.com>
2020-07-01 16:36:19 +0900

    Create triang.py

---
+++ triang.py
@@ -0,0 +1,22 @@
+#This python scirpt print like below(triangle line == n)
+#                   *
+#                 *   *
+#               *   *   *
+#             *   *   *   *
+#           *   *   *   *   *
+#         *   *   *   *   *   *
+#       *   *   *   *   *   *   *
+#     *   *   *   *   *   *   *   *
+#   *   *   *   *   *   *   *   *   *
+def tri(n):
+    s = 2*n
+    for i in range(1,n+1):
+        sp = s*" "
+        for j in range(i):
+            if j == 0:
+               print(sp,"*",end='')
+            else:
+               print(" "*2,"*",end='')
+        s=s-2
+        print()
+tri(9)
