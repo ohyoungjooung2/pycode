b65d5ce4b44969d02118bdd01b9a554fa20d3f90
ohyoungjooung <wnapdlf05@gmail.com>
2020-07-25 13:14:20 +0900

    Update staircase.py

--- staircase.py
+++ staircase.py
@@ -1,16 +1,16 @@
 #This script print like below of (4) value
-#   #
-#  ##
-# ###
-#####
+#   *
+#  **
+# ***
+#****
 def scase(n):
     for i in range(1,n+1):
         sp=' '*(n-i)
         for j in range(i):
           if j == 0:          
-             print(sp+'#',end='')
+             print(sp+'*',end='')
           else:
-             print('#',end='')
+             print('*',end='')
 
         print()
 scase(5)
