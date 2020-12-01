d5b19fc8da39f6c5d736aabb99e2ea86df080971
ohyoungjooung <wnapdlf05@gmail.com>
2020-12-02 00:34:16 +0900

    Add files via upload

--- split.py
+++ split.py
@@ -1,12 +1,23 @@
 #This script split binary file into 1mb
 #Here just test.mp3. Should change this file name.
 #Also CH_SIZE can be changed to bigger or smaller
-CH_SIZE=1048576
+#This is binary file splitter
+import sys
+import os
+CH_SIZE=int(sys.argv[1])
+FILENAME=str(sys.argv[2])
+OUTPUT="./"+FILENAME+"_"+"output"
+CUDIR=os.getcwd()
+if os.path.exists(OUTPUT)==False:
+    os.mkdir(OUTPUT)
+
 file_number=1
-with open('test.mp3','br') as f:
+with open(FILENAME,'br') as f:
     chunk = f.read(CH_SIZE)
     while chunk:
-        with open('test.mp3_part_'+str(file_number),'bw') as chk_file:
+        os.chdir(OUTPUT)
+        with open(FILENAME+'_part_'+str(file_number),'bw') as chk_file:
             chk_file.write(chunk)
+        os.chdir(CUDIR)
         file_number += 1
         chunk = f.read(CH_SIZE)
