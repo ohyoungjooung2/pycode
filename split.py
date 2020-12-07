1195058028a6d106e813718d0fc26ad00c4b2f13
ohyoungjooung <wnapdlf05@gmail.com>
2020-12-07 23:52:37 +0900

    Update split.py

--- split.py
+++ split.py
@@ -4,8 +4,9 @@
 #This is binary file splitter
 import sys
 import os
-CH_SIZE=int(sys.argv[1])
+import hashlib
 FILENAME=str(sys.argv[2])
+CH_SIZE=int(sys.argv[1])
 OUTPUT="./"+FILENAME+"_"+"output"
 CUDIR=os.getcwd()
 if os.path.exists(OUTPUT)==False:
@@ -16,8 +17,13 @@ with open(FILENAME,'br') as f:
     chunk = f.read(CH_SIZE)
     while chunk:
         os.chdir(OUTPUT)
-        with open(FILENAME+'_part_'+str(file_number),'bw') as chk_file:
+        #with open(FILENAME+'_part_'+str(%03d %(file_number)),'bw') as chk_file:
+        with open(FILENAME+'_part_'+ ("%03d" %(file_number)),'bw') as chk_file:
             chk_file.write(chunk)
         os.chdir(CUDIR)
         file_number += 1
         chunk = f.read(CH_SIZE)
+
+print("Split is over")
+FILEMD5=hashlib.md5(open(FILENAME,'rb').read()).hexdigest()
+print(f"md5 { sys.argv[2] } is {FILEMD5}")
