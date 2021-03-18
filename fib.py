7305f1e4617e9dfa9a3d07a9ec09af700ff84610
oyj <oyj@local>
2021-03-19 00:01:05 +0900

    fun str list fibonacci result

---
+++ fib.py
@@ -0,0 +1,26 @@
+#Simple fibonacci
+def fib(n):
+  fir=0
+  secd=1
+  rs=[]
+  s=""
+  print(fir) #0
+  print(secd) #1
+  rs.append(fir)
+  s+=str(fir)
+  s+=" "
+  rs.append(secd)
+  s+=str(secd)
+  s+=" "
+  for i in range(n):
+      temp=fir #temp=0,1,1,2
+      fir=secd #fir=1,1,2,3
+      secd=temp+fir #secd=1,2,3,5
+      print(secd) #print(1,2,3,5))-(0,1,1,2,3,5)
+      rs.append(secd)
+      s+=str(secd)
+      s+=" "
+  print(rs)
+  print(s)
+
+fib(10)
