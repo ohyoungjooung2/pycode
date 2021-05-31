fbdbbda867a7959c5e34c97cea3bba62050bfa31
Your Name <you@example.com>
2021-06-01 03:02:51 +0900

    https://www.hackerrank.com/challenges/cut-the-sticks/problem

--- stick.py
+++ stick.py
@@ -18,6 +18,32 @@ a=[1,2,2,3,3,4]
 a=[5,4,4,2,2,8]
 
 print(st(a))
-        
-    
-
+#package tst;
+#
+#import java.util.Scanner;
+#import java.util.Arrays;
+#public class Solution {
+#	public static void main(String[] args) {
+#		/* Save Input */
+#		Scanner scan = new Scanner(System.in);
+#		int numSticks = scan.nextInt();
+#		int [] array = new int[numSticks];
+#		for (int i = 0; i < numSticks; i++) {
+#			array[i] = scan.nextInt();
+#		}
+#		scan.close();
+#		Arrays.sort(array);
+#        System.out.println(array.length);
+#		for (int i = 1; i < array.length; i++) {
+#			//System.out.println(i);
+#			//System.out.println(array[i]);
+#			if (array[i] != array[i-1]) {
+#				System.out.println(array.length-i);
+#
+#			}
+#			
+#		}
+#				
+#	}
+#
+#}
